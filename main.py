b50f8693ce4134db85a2545306f92c3a329a81b9
Cameron Dugan <cameron.dugan@protonmail.com>
2024-01-25 16:23:36 -0500

    added base classes

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+class location:
+    def __init__(self, name):
+        self.name = name
+
+    name = []
+    adjLocations = []
+    interactables = []
+
+
+class interactable:
+    name = "interactable"
+    desc = "description of interactable"
+    actions = ["use"]
+    actionAliases = {}
+
+
+class item:
+    name = "item"
+
+
+class player:
+    currentLocation: location = location("null")
+
+
+def buildWorld():
+    you = player
+    mars = location("Mars")
+    you.currentLocation = mars
+    return you
+
+
 def main():
-    print("Welcome to main.py")
+    you = buildWorld()
+    print("You are on", you.currentLocation.name)
 
 
 if __name__ == "__main__":
