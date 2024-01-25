69309820ac4812e96cbf97c3d5f9533e6ecd58ce
Cameron Dugan <cameron.dugan@protonmail.com>
2024-01-25 16:31:35 -0500

    added game loop

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 class location:
-    def __init__(self, name):
+    def __init__(self, name, desc):
         self.name = name
+        self.description = desc
 
-    name = []
+    def showPlayer(self):
+        print(self.name + " " + self.description)
+        if len(self.interactables) != 0:
+            print("Objects nearby:")
+            for item in self.interactables:
+                print("    " + item.name)
+
+    name = ""
+    description = ""
     adjLocations = []
     interactables = []
 
@@ -19,12 +28,15 @@ class item:
 
 
 class player:
-    currentLocation: location = location("null")
+    currentLocation: location = location("null", "")
+    alive: bool = True
 
 
 def buildWorld():
     you = player
-    mars = location("Mars")
+    mars = location("Mars", "the planet")
+    lander = location("lander", "your space ship (no fuel)")
+    mars.adjLocations = [lander]
     you.currentLocation = mars
     return you
 
@@ -32,6 +44,9 @@ def buildWorld():
 def main():
     you = buildWorld()
     print("You are on", you.currentLocation.name)
+    while you.alive:
+        you.currentLocation.showPlayer()
+        action = input()
 
 
 if __name__ == "__main__":
