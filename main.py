d9fffcace21487257944354ed7bca8abc61a8a9a
Cameron Dugan <cameron.dugan@protonmail.com>
2024-01-30 16:51:23 -0500

    added colors hopefully cross-platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 from colorOutput import *
-from colorOutput import prLightPurple
 
 
 class Location:
@@ -20,9 +19,9 @@ class Location:
                 if not inter.hidden:
                     prCyan("    - " + inter.name)
         if len(self.adjLocations) > 0:
-            prLightPurple("Nearby locations: ")
+            prBlue("Nearby locations: ")
             for loc in self.adjLocations:
-                prLightPurple(f"    - {loc.name}")
+                prBlue(f"    - {loc.name}")
 
     def isName(self, name: str) -> bool:
         return name == self.name or name in self.aliases
